5efb297686cd04796c8b587d4405147ff1a327a7
romerofm <romerocontato7@gmail.com>
2024-09-13 14:13:05 -0300

    tabela p

--- webix.js
+++ webix.js
@@ -1,1 +1,12 @@
+webix.ready(function(){
+    webix.ui({
+        rows:[
+          { template:"Linha 1" },
+          { cols:[
+            { template:"Linha 2 coluna 1" },
+            { template:"Linha 2 coluna 1" }
+          ]}
+        ]
+      });
 
+});
